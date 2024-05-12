0dca10325f6a86eb184fe432a1717cda73283b72
Evan Xu <evan.xuqin@gmail.com>
2024-05-12 21:41:02 +1000

    Implement generator

--- src/main/java/com/zeroonejourney/textgen/MarkovTextGeneratorLoL.java
+++ src/main/java/com/zeroonejourney/textgen/MarkovTextGeneratorLoL.java
@@ -11,14 +11,13 @@ import java.util.Random;
  */
 public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 
+  public static final String SPACE = " ";
   // The list of words with their next words
   private List<ListNode> wordList;
-
+  // The random number generator
+  private final Random rnGenerator;
   // The starting "word"
   private String starter;
-
-  // The random number generator
-  private Random rnGenerator;
 
   public MarkovTextGeneratorLoL(Random generator) {
     wordList = new LinkedList<ListNode>();
@@ -26,48 +25,16 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
     rnGenerator = generator;
   }
 
-  /** Train the generator by adding the sourceText */
-  @Override
-  public void train(String sourceText) {
-    // TODO: Implement this method
-  }
-
-  /** Generate the number of words requested. */
-  @Override
-  public String generateText(int numWords) {
-    // TODO: Implement this method
-    return null;
-  }
-
-  // Can be helpful for debugging
-  @Override
-  public String toString() {
-    String toReturn = "";
-    for (ListNode n : wordList) {
-      toReturn += n.toString();
-    }
-    return toReturn;
-  }
-
-  /** Retrain the generator from scratch on the source text */
-  @Override
-  public void retrain(String sourceText) {
-    // TODO: Implement this method.
-  }
-
-  // TODO: Add any private helper methods you need here.
-
   /**
    * This is a minimal set of tests. Note that it can be difficult to test methods/classes with
    * randomized behavior.
    *
-   * @param args
+   * @param args the arguments
    */
   public static void main(String[] args) {
     // feed the generator a fixed random value for repeatable behavior
     MarkovTextGeneratorLoL gen = new MarkovTextGeneratorLoL(new Random(42));
-    String textString =
-        "Hello.  Hello there.  This is a test.  Hello there.  Hello Bob.  Test again.";
+    String textString = "Hello.  Hello there.  This is a test.  Hello there.  Hello Bob.  Test again.";
     System.out.println(textString);
     gen.train(textString);
     System.out.println(gen);
@@ -101,20 +68,109 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
     gen.retrain(textString2);
     System.out.println(gen);
     System.out.println(gen.generateText(20));
+
+    String textString3 = "hi hi hi hello";
+    System.out.println(textString3);
+    gen.retrain(textString3);
+    System.out.println(gen);
+    System.out.println(gen.generateText(20));
+  }
+
+  public List<ListNode> getWordList() {
+    return wordList;
+  }
+
+  /**
+   * Train the generator by adding the sourceText.
+   */
+  @Override
+  public void train(String sourceText) {
+    String[] words = sourceText.split(" ");
+    starter = words[0];
+    String prevWord = starter;
+    wordList.add(new ListNode(starter));
+    for (int i = 1; i <= words.length; i++) {
+      String w;
+      if (i == words.length) {
+        w = starter;
+      } else {
+        w = words[i];
+      }
+      boolean wordFound = false;
+      for (ListNode ln : wordList) {
+        if (ln.getWord().equals(prevWord)) {
+          ln.addNextWord(w);
+          wordFound = true;
+          break;
+        }
+      }
+      if (!wordFound) {
+        ListNode n = new ListNode(prevWord);
+        n.addNextWord(w);
+        wordList.add(n);
+      }
+      prevWord = w;
+    }
+  }
+
+  /**
+   * Generate the number of words requested.
+   */
+  @Override
+  public String generateText(int numWords) {
+    String currWord = starter;
+    String output = "";
+    output = currWord + SPACE;
+    int count = 0;
+    while(count < numWords) {
+      for (ListNode wl : wordList) {
+        if (wl.getWord().equals(currWord)) {
+          String w = wl.getRandomNextWord(rnGenerator);
+          output += w + " ";
+          currWord = w;
+          count++;
+        }
+      }
+    }
+    return output;
+  }
+
+  @Override
+  public String toString() {
+    String toReturn = "";
+    for (ListNode n : wordList) {
+      toReturn += n.toString();
+    }
+    return toReturn;
+  }
+
+  /**
+   * Retrain the generator from scratch on the source text.
+   */
+  @Override
+  public void retrain(String sourceText) {
+    wordList = new LinkedList<>();
+    starter = "";
+    train(sourceText);
   }
 }
 
-/** Links a word to the next words in the list You should use this class in your implementation. */
+/**
+ * Links a word to the next words in the list You should use this class in your implementation.
+ */
 class ListNode {
   // The word that is linking to the next words
-  private String word;
-
+  private final String word;
   // The next words that could follow it
-  private List<String> nextWords;
+  private final List<String> nextWords;
 
   ListNode(String word) {
     this.word = word;
     nextWords = new LinkedList<String>();
+  }
+
+  public List<String> getNextWords() {
+    return nextWords;
   }
 
   public String getWord() {
@@ -126,10 +182,7 @@ class ListNode {
   }
 
   public String getRandomNextWord(Random generator) {
-    // TODO: Implement this method
-    // The random number generator should be passed from
-    // the MarkovTextGeneratorLoL class
-    return null;
+    return nextWords.get(generator.nextInt(nextWords.size()));
   }
 
   public String toString() {
---
+++ src/test/java/com/zeroonejourney/textgen/MarkovTextGeneratorLoLTest.java
@@ -0,0 +1,31 @@
+package com.zeroonejourney.textgen;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Random;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MarkovTextGeneratorLoLTest {
+  private final MarkovTextGeneratorLoL generator = new MarkovTextGeneratorLoL(new Random(42));
+
+  @Before
+  public void setUp() throws Exception {
+    String textString = "hi there hi Leo";
+    System.out.println(textString);
+    generator.train(textString);
+  }
+
+  @Test
+  public void testTrain() {
+    List<ListNode> wordList = generator.getWordList();
+    assertEquals("hi", wordList.get(0).getWord());
+    assertEquals("there", wordList.get(0).getNextWords().get(0));
+    assertEquals("Leo", wordList.get(0).getNextWords().get(1));
+    assertEquals("there", wordList.get(1).getWord());
+    assertEquals("hi", wordList.get(1).getNextWords().get(0));
+    assertEquals("Leo", wordList.get(2).getWord());
+    assertEquals("hi", wordList.get(2).getNextWords().get(0));
+  }
+}
