2f78143c12827ec13c5d6d5392bafae3a335412d
Evan Xu <evan.xuqin@gmail.com>
2024-09-28 16:12:24 +1000

    Implement dictionary BST

--- src/main/java/com/zeroonejourney/spelling/DictionaryBST.java
+++ src/main/java/com/zeroonejourney/spelling/DictionaryBST.java
@@ -8,29 +8,37 @@ import java.util.TreeSet;
 public class DictionaryBST implements Dictionary {
   private TreeSet<String> dict;
 
-  // TODO: Implement the dictionary interface using a TreeSet.
-  // You'll need a constructor here
+  public DictionaryBST() {
+    dict = new TreeSet<String>();
+  }
 
   /**
-   * Add this word to the dictionary. Convert it to lowercase first for the assignment requirements.
+   * Add this word to the dictionary. Convert it to lowercase first for the
+   * assignment requirements.
    *
    * @param word The word to add
-   * @return true if the word was added to the dictionary (it wasn't already there).
+   * @return true if the word was added to the dictionary (it wasn't already
+   *         there).
    */
   public boolean addWord(String word) {
-    // TODO: Implement this method
-    return false;
+    String w = word.toLowerCase();
+    if (dict.contains(w)) {
+      return false;
+    }
+    return dict.add(w);
   }
 
-  /** Return the number of words in the dictionary */
+  /**
+   * Return the number of words in the dictionary.
+   */
   public int size() {
-    // TODO: Implement this method
-    return 0;
+    return dict.size();
   }
 
-  /** Is this a word according to this dictionary? */
+  /**
+   * Is this a word according to this dictionary?
+   */
   public boolean isWord(String s) {
-    // TODO: Implement this method
-    return false;
+    return dict.contains(s.toLowerCase());
   }
 }
